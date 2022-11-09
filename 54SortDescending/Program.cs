e0335a526032323177ba8c9a122576d00efd6c8c
Tatyana <tatyana.bazh17@gmail.com>
2022-11-09 22:49:39 +0400

    решена 54 задача по С# д/з №8

---
+++ 54SortDescending/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortRow(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int jTemp = j + 1; jTemp < inArray.GetLength(1); jTemp++)
+            {
+                if (inArray[i, j] < inArray[i, jTemp])
+                {
+                    int temp = inArray[i, j];
+                    inArray[i, j] = inArray[i, jTemp];
+                    inArray[i, jTemp] = temp;
+                }
+            }
+
+        }
+    }
+}
+
+Console.Write("Введите количество строк в массиве:");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве:");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] userArray = GetArray(row, col, 0, 10);
+PrintArray(userArray);
+Console.WriteLine();
+
+SortRow(userArray);
+PrintArray(userArray);
