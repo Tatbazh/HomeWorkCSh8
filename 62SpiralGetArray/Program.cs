d110a3f3433cf71f4021552da3660d55d459564c
Tatyana <tatyana.bazh17@gmail.com>
2022-11-14 22:18:13 +0400

    решены все задачи д/з №8 Рекурсии C#

--- 62SpiralGetArray/Program.cs
+++ 62SpiralGetArray/Program.cs
@@ -1,12 +1,43 @@
 ﻿// Задача 62: Заполните спирально массив 4 на 4.
+
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
     int[,] result = new int[m, n];
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
+
+    int sortedsize = 0;
+    int row = 0;
+    int column = 0;
+
+    while (minValue < m * n)
+    {
+        while (column < n - sortedsize)  // слева направо
         {
-            if ()
+            result[row, column] = minValue++;
+            column++;
         }
+        column = n - sortedsize - 1;
+        row = sortedsize +1;
+        while (row < m - sortedsize) // сверху вниз
+        {
+            result[row, column] = minValue++;
+            row++;
+        }
+        column = n - sortedsize - 2;
+        row = m - 1 - sortedsize;
+        while (column > sortedsize - 1) // справа налево
+        {
+            result[row, column] = minValue++;
+            column--;
+        }
+        column = sortedsize;
+        row = m - sortedsize - 2;
+        while (row > sortedsize)
+        {
+            result[row, column] = minValue++;
+            row--;
+            sortedsize++;
+        }
+    }
     return result;
 }
 
@@ -22,6 +53,6 @@ void PrintArray(int[,] inArray)
     }
 }
 
-int[,] userArray = GetArray(4, 4, 0, 16);
+int[,] userArray = GetArray(4, 4, 1, 16);
 PrintArray(userArray);
 Console.WriteLine();
